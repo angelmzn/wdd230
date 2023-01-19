0dbf1dc907ceaff8d48f7e37ed97a604410696db
ANGEL_MANZANO <angel.manzano777@hotmail.com>
2023-01-18 22:15:29 -0600

    Design Principles

--- Lesson2/Scripts/script.js
+++ Lesson2/Scripts/script.js
@@ -1,7 +1,7 @@
+
 const date = new Date(document.lastModified);
 const lastModifiedDiv = document.querySelector('#lastupdated');
 lastModifiedDiv.innerHTML = date;
-
 
 //Gets the span we are trying to modify with the Query Selector method.
 const dateDiv = document.querySelector('#date')
@@ -11,3 +11,6 @@ let year =  new Date().getFullYear();
 console.log(date);
 // Add the current year to the text
 dateDiv.textContent = year;
+
+
+
