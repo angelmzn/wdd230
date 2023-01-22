3ac18e5096fde5d3fe409eaf8f94bd76294b94b8
ANGEL_MANZANO <angel.manzano777@hotmail.com>
2023-01-22 14:53:49 -0600

    Chamber of commerce activity lesson 3 submission

--- lesson3/scripts/script.js
+++ lesson3/scripts/script.js
@@ -1,10 +1,10 @@
 const date = new Date(document.lastModified);
-const lastModifiedDiv = document.querySelector('#lastupdated');
-lastModifiedDiv.innerHTML = date.getMonth() + 1 + "th, " + date.getDate() + ", "+ date.getFullYear();
+const lastModifiedDiv = document.querySelector('#last-updated');
+lastModifiedDiv.innerHTML = date.getMonth() + 1 + "-" + date.getDate() + "-"+ date.getFullYear();
 
 
 //Gets the span we are trying to modify with the Query Selector method.
-const dateDiv = document.querySelector('#date')
+const dateDiv = document.querySelector('#year')
 
 //Date().getFullYear()
 let year =  new Date().getFullYear();
