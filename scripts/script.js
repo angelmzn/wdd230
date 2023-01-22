30444e3a2688f63e1e2b647ec4a50b41f5200080
ANGEL_MANZANO <angel.manzano777@hotmail.com>
2023-01-22 08:52:14 -0600

    removimg console.log messages

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const date = new Date(document.lastModified);
 const lastModifiedDiv = document.querySelector('#lastupdated');
-lastModifiedDiv.innerHTML = date;
+lastModifiedDiv.innerHTML = date.getDate() + "th, " + date.getMonth() + 1 + ", "+ date.getFullYear();
 
 
 //Gets the span we are trying to modify with the Query Selector method.
@@ -8,6 +8,5 @@ const dateDiv = document.querySelector('#date')
 
 //Date().getFullYear()
 let year =  new Date().getFullYear();
-console.log(date);
 // Add the current year to the text
 dateDiv.textContent = year;
